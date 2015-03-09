c627d15ddaf8f28ea2b1431a72c3b0826ecc9195
Edwin Ortiz <eortiz@udel.edu>
2015-03-09 16:39:54 -0400

    updated triangle

--- src/problem16/Triangle.java
+++ src/problem16/Triangle.java
@@ -9,7 +9,11 @@ public abstract class Triangle extends GeometricObject {
 	public Triangle() { }
 	
 	// 
-	public Triangle(int side1, int side2, int side3) { }
+	public Triangle(int side1, int side2, int side3) {
+		this.side1 = side1;
+		this.side2 = side2;
+		this.side3 = side3;
+	}
 	 
 	 
 		public double getSide1() {
